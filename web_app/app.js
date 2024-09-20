23cf8ae8bd4ded89ca5b8515a63c18f3831d9c8c
ErolKocoglu <erolkocoglu44@gmail.com>
2024-09-20 17:06:54 +0300

    add ejs template

--- web_app/app.js
+++ web_app/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const path =require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req,res)=>{
-    res.send("<h1> Welcome to the home page</h1>");
+    res.render("home");
     console.log("A request has come!");
+});
+
+app.get("/:word", (req,res)=>{
+    const {word} =req.params;
+    res.render("home",{word});
 })
+
+app.get("/student/:id", (req, res) =>{
+    const {id} = req.params;
+    console.log(req.params);
+    res.send(`<h2>Results for ${id}</h2>`);
+});
 
 app.listen(8080, ()=>{
     console.log("Listening on port 8080");
-})
+});
