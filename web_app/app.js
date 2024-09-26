9f636a385f4ae78161c77e68d9590ad72dab58a0
ErolKocoglu <erolkocoglu44@gmail.com>
2024-09-26 20:00:01 +0300

    try forms to search

--- web_app/app.js
+++ web_app/app.js
@@ -12,16 +12,24 @@ app.get("/", (req,res)=>{
     console.log("A request has come!");
 });
 
+app.get("/search", (req,res) =>{
+    res.render("form");
+});
+
+app.get("/student", (req, res) =>{
+    const {id} = req.query;
+    console.log(req.query);
+    res.send(`<h2>Results for ${id}</h2>`);
+});
+
 app.get("/:word", (req,res)=>{
     const {word} =req.params;
     res.render("home",{word});
 })
 
-app.get("/student/:id", (req, res) =>{
-    const {id} = req.params;
-    console.log(req.params);
-    res.send(`<h2>Results for ${id}</h2>`);
-});
+
+
+
 
 app.listen(8080, ()=>{
     console.log("Listening on port 8080");
