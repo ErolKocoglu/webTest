2b2fc05d8a783183dc628a14a23663193b9c4613
ErolKocoglu <erolkocoglu44@gmail.com>
2024-09-29 21:59:32 +0300

    add all students page

--- web_app/app.js
+++ web_app/app.js
@@ -7,6 +7,30 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+let students = [
+    {
+        id:1,
+        name:"John Doe",
+        gpa: 3.90 
+    },
+    {
+        id:2,
+        name:"Kit Walker",
+        gpa: 3.65
+    },
+    {
+        id:3,
+        name:"Jude Martin",
+        gpa:3.42
+    },
+    {
+        id:4,
+        name: "Will Graham",
+        gpa: 2.95
+    }
+
+]
+
 app.get("/", (req,res)=>{
     res.render("home");
     console.log("A request has come!");
@@ -16,10 +40,15 @@ app.get("/search", (req,res) =>{
     res.render("form");
 });
 
-app.get("/student", (req, res) =>{
-    const {id} = req.query;
-    console.log(req.query);
-    res.send(`<h2>Results for ${id}</h2>`);
+app.get("/students", (req, res) =>{
+    if(!(Object.keys(req.query).length === 0)){
+        const {id} = req.query;
+        console.log(req.query);
+        res.send(`<h2>Results for ${id}</h2>`);
+    }else{
+        res.render("students",{students});
+    }
+    
 });
 
 app.get("/:word", (req,res)=>{
